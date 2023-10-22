a9ae240a127133661f6cf99f196740ecf2169b56
carolynl23 <56048088+carolynl23@users.noreply.github.com>
2023-10-22 14:03:48 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+
 let current = 0
 const panels = document.getElementById("allPanels").children
 const cont = document.getElementById("continue")
 document.addEventListener('keydown',typeInCells());
-
 for (let i = 1; i < panels.length; i++) {
     panels[i].style.display = "none"
 }
@@ -12,16 +12,13 @@ function next() {
     panels[current].style.display = "none"
     current++
     panels[current].style.display = "block"
-
     if (panels[current].classList.contains("locked")) {
         cont.style.display = "none"
     }
 }
-
 function elev1() {
     elevFail()
 }
-
 function elev2() {
     if (document.getElementById("elevSucc").style.display === "block")
         return
@@ -29,21 +26,17 @@ function elev2() {
     document.getElementById("elevSucc").style.display = "block"
     document.getElementById("elevError").remove()
 }
-
 function elev3() {
     elevFail()
 }
-
 function elevFail() {
     let bar = document.getElementById("elevError")
     if (bar != null)
         bar.style.display = "block"
 }
-
 function getInput(){
     document.addEventListener('keydown',typeInCells());
 }
-
 function typeInCells(){
     //cells of grid and index of box to type letter
     let cells = document.getElementById('passInput').children
@@ -53,7 +46,6 @@ function typeInCells(){
     let isAlpha = (ch) =>{
         if(ch === undefined || ch === null || ch.length > 1)
             return false
-
       return (/[a-zA-Z1-9]/).test(ch)
     }  
     
@@ -72,15 +64,66 @@ function typeInCells(){
                 string+=char
                 cells[i].innerHTML = char
             }
-
-            if (string === "MONEY") {
+            if (string === "FORCE") {
                 cont.style.display = "block"
                 document.removeEventListener('keydown', handler)
             }
         }
     }
 }
-
+const input3 = document.getElementById("puzzle3").children
+const ans3 = [7193396, 9411920]
+let currentQ = 0
+function check(x) {
+    if(event.key === 'Enter') {
+        if (+x.value === ans3[currentQ]) {
+            if (currentQ===1) {
+                input3[1].disabled = true
+                cont.style.display = "block"
+            }
+            else {
+                input3[currentQ].style.display = "none"
+                currentQ++
+                input3[currentQ].style.display = "block"
+                input3[currentQ].focus()
+            }
+        } else {
+            console.log("anthony is smmart")
+            input3[currentQ].classList.add("shake")
+            setTimeout(function () {
+                input3[currentQ].classList.remove("shake")
+                input3[currentQ].style.transform = "translate3d(0, 0, 0)"
+            }, 500)
+        }
+    }
+}
+const input4 = document.getElementById("puzzle4").children
+// const ans4 = [1,2]
+const ans4 = [31, 60]
+let currentQ2 = 0
+function checkV2(x){
+       if(event.key === 'Enter'){
+           if (+x.value === ans4[currentQ2]) {
+                if(currentQ2 === 1){
+                    input4[1].children[1].disabled = true
+                    cont.style.display = "block"
+                }
+               else{
+                    input4[currentQ2].style.display = "none"
+                    currentQ2++
+                    input4[currentQ2].style.display = "block"
+                    input4[currentQ2].children[1].focus()
+               }
+           } else {
+            console.log("anthony is smmart")
+            input4[currentQ2].classList.add("shake")
+            setTimeout(function () {
+                input4[currentQ2].classList.remove("shake")
+                input4[currentQ2].style.transform = "translate3d(0, 0, 0)"
+            }, 500)
+        }
+       }
+}
 
 function checkWork(x) {
     let mult = document.getElementById("multiply2numbers")
@@ -96,10 +139,8 @@ function checkWork(x) {
                 mult.style.transform = "translate3d(0, 0, 0)"
             }, 500)
         }
-
     }
 }
-
 function showHint(id, button) {
     document.getElementById(id).style.display = "block"
     button.style.display = "none"
